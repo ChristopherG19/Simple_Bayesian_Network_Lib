800cfd96d26dd3eddfb790d327786dbafd44880e
Gabriel Alejandro Vicente Lorenzo <60375344+GabrielVicente-GT@users.noreply.github.com>
2023-02-07 18:25:50 -0600

    Aditional Functions BayesianNetwork

--- BayesianNetwork/BayesianNetwork.py
+++ BayesianNetwork/BayesianNetwork.py
@@ -1,3 +1,8 @@
+#Inteligencia Artificial Laboratorio 2
+
+#Chrisopher Garcia 20541
+#Gabriel Vicente 20498
+
 class Node:
     def __init__(self, name, states, parents=None, cpt=None):
         self.name = name
@@ -12,13 +17,34 @@ class Node:
         self.cpt[parent_combination] = cpt_values
 
     def __str__(self):
-        linea = "Nombre = "+str(self.name) + "CPT = "+ str(self.cpt)
-        return linea
+        return "P("+str(self.name)+")"+": "+ str(self.cpt)+"\nParents: "+str([node.name for node in self.parents])+"\n"
 
 
 class BayesianNetwork:
     def __init__(self, nodes):
         self.nodes = nodes
+
+    def isFullyDescribed(self):
+        for node in self.nodes:
+            for parent_states in node.cpt:
+                for state in node.states:
+                    if state not in node.cpt[parent_states]:
+                        return False
+        return True
+        
+    def compactForm(self):
+        StringResult = "\n"
+        for node in self.nodes:
+            StringResult += str(node)+"\n"
+            
+        return StringResult
+        
+    def elementsNetwork(self):
+        result = {}
+        for node in self.nodes:
+            result[node.name] = node.cpt
+            
+        return result
 
     def joint_probability(self, event):
         jp = 1
@@ -30,3 +56,4 @@ class BayesianNetwork:
             else:
                 jp *= node.cpt[node_state]
         return jp
+
